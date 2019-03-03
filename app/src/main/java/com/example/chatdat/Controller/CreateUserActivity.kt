11ab613ee4bcdfeb3770d6191dca0e4afcec611a
unknown <shivamk522@gmail.com>
2019-03-03 10:52:23 +0530

    login user

--- app/src/main/java/com/example/chatdat/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/example/chatdat/Controller/CreateUserActivity.kt
@@ -58,7 +58,22 @@ class CreateUserActivity : AppCompatActivity() {
 
     fun createUserClicked(view: View)
     {
-        AuthService.registerUser(this,"j@j.com", "123456"){}
+        val email = createEmailText.text.toString()
+        val password = createPasswordText.text.toString()
+
+        AuthService.registerUser(this, email, password){ registerSuccess ->
+            if (registerSuccess)
+            {
+                AuthService.loginUser(this, email, password){loginSuccess ->
+                    if(loginSuccess)
+                    {
+                        println(AuthService.authToken)
+                        println(AuthService.userEmail)
+
+                    }
+                }
+            }
+        }
 
     }
 }
