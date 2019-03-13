28509f9f1245dfc01644b7da3a9edea713c001c7
unknown <shivamk522@gmail.com>
2019-03-13 11:06:28 +0530

    Add Channel Dialog

--- app/src/main/java/com/example/chatdat/Controller/LoginActivity.kt
+++ app/src/main/java/com/example/chatdat/Controller/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.example.chatdat.Controller
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import com.example.chatdat.R
 import com.example.chatdat.Services.AuthService
 import kotlinx.android.synthetic.main.activity_login.*
@@ -13,6 +16,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        loginSpinner.visibility = View.INVISIBLE
     }
 
     fun loginCreateUserBtnClicked(view: View)
@@ -25,22 +29,64 @@ class LoginActivity : AppCompatActivity() {
 
     fun loginLoginBtnClicked(view: View)
     {
+        enableSpinner(true)
         val email = loginEmailTxt.text.toString()
         val password = loginPasswordTxt.text.toString()
 
-        AuthService.loginUser(this, email, password) {loginSuccess ->
-            if (loginSuccess)
-            {
-                AuthService.findUserByEmail(this) {findSuccess ->
-                    if (findSuccess)
-                    {
-                        finish()
-                    }
+        hideKeyboard()
 
+        if(email.isNotEmpty() && password.isNotEmpty())
+        {
+            AuthService.loginUser(this, email, password) {loginSuccess ->
+                if (loginSuccess)
+                {
+                    AuthService.findUserByEmail(this) {findSuccess ->
+                        if (findSuccess)
+                        {
+                            enableSpinner(false)
+                            finish()
+                        } else
+                        {
+                            errorToast()
+                        }
+
+                    }
+                } else
+                {
+                    errorToast()
                 }
             }
-
+        } else
+        {
+            Toast.makeText(this, "Please fill in both email and password", Toast.LENGTH_LONG).show()
         }
 
+
+
+    }
+
+    fun errorToast() {
+        Toast.makeText(this, "Something went wrong, please try again.", Toast.LENGTH_LONG).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner(enable: Boolean) {
+        if (enable)
+        {
+            loginSpinner.visibility = View.VISIBLE
+        } else
+        {
+            loginSpinner.visibility = View.INVISIBLE
+        }
+        loginLoginBtn.isEnabled = !enable
+        loginCreateUserBtn.isEnabled = !enable
+    }
+
+    fun hideKeyboard() {
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        if(inputManager.isAcceptingText) {
+            inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+        }
     }
 }
--- app/src/main/java/com/example/chatdat/Controller/MainActivity.kt
+++ app/src/main/java/com/example/chatdat/Controller/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.example.chatdat.Controller
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
+import android.content.*
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.content.LocalBroadcastManager
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import com.example.chatdat.R
 import com.example.chatdat.Services.AuthService
 import com.example.chatdat.Services.UserDataService
@@ -34,6 +34,7 @@ class MainActivity : AppCompatActivity() {
         )
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
+        hideKeyboard()
 
         LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver, IntentFilter(
             BROADCAT_USER_DATA_CHANGE))
@@ -88,11 +89,41 @@ class MainActivity : AppCompatActivity() {
 
     fun addChannelClicked(view: View)
     {
+        if(AuthService.isLoggedIn)
+        {
+            val builder = AlertDialog.Builder(this)
+            val dialogView = layoutInflater.inflate(R.layout.add_channel_dialog, null)
+
+            builder.setView(dialogView)
+                   .setPositiveButton("Add") { dialogInterface, i ->
+                       // perform some logic when clicked
+                       val nameTextField = dialogView.findViewById<EditText>(R.id.addChannelNameTxt)
+                       val descTextField = dialogView.findViewById<EditText>(R.id.addChannelDescTxt)
+                       val channelName = nameTextField.text.toString()
+                       val channelDesc = descTextField.text.toString()
+
+                       // Create channnel eith the channel name and description
+                       hideKeyboard()
+                   }
+                   .setNegativeButton("Cancel") { dialogInterface, i ->
+                       //Cancel and close the dialog
+                       hideKeyboard()
+                   }
+                   .show()
+        }
 
     }
 
     fun sendMsgBtnCLicked(view: View)
     {
 
+    }
+
+    fun hideKeyboard() {
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        if(inputManager.isAcceptingText) {
+            inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+        }
     }
 }
